35f984bc885acc294ca62a6438dc376153ff7e16
Jundan Zhang <1750132640@qq.com>
2023-04-20 21:06:10 +0800

    Update: Login page UI

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,89 +1,89 @@
-import { Button, Checkbox, Form, Input, Card } from 'antd';
+import { Button, Form, Input, Card } from 'antd';
+import { CloudTwoTone, LockOutlined, IdcardOutlined } from '@ant-design/icons';
 import styles from './index.less';
+import { useState } from 'react';
+import { intl } from '../../locales/intl';
 
 export default function Login() {
+  const [input, setInput] = useState({
+    account: '',
+    password: '',
+  });
+
   const onFinish = (values) => {
     console.log('Success:', values);
   };
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
+  
   return (
     <div className={styles.card}>
       <div className={styles.container}>
-      <Card
-        style={{
-          width: 400,
-          height: 500
-        }}
-      >
-        <Form
-          name="basic"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
+        <Card
           style={{
-            maxWidth: 600,
+            width: 400,
+            height: 500,
           }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
         >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your username!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
+          <div className={styles.form}>
+            <div className={styles.logo}>
+              <CloudTwoTone style={{ fontSize: '80px' }} />
+              <div className={styles.title}>{intl('pages.login.title')}</div>
+            </div>
+            <Form
+              name="login"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              style={{
+                maxWidth: 600,
+                marginTop: 64,
+              }}
+              onFinish={onFinish}
+            >
+              <Form.Item name="username">
+                <Input
+                  size="large"
+                  placeholder={intl('pages.login.accountMsg')}
+                  style={{ width: 256 }}
+                  prefix={<IdcardOutlined />}
+                  onChange={(e)=>{
+                    setInput({
+                      ...input,
+                      account: e.target.value,
+                    })
+                  }}
+                />
+              </Form.Item>
+              <Form.Item name="password">
+                <Input.Password
+                  size="large"
+                  placeholder={intl('pages.login.passwordMsg')}
+                  prefix={<LockOutlined />}
+                  style={{ width: 256 }}
+                  onChange={(e)=>{
+                    setInput({
+                      ...input,
+                      password: e.target.value,
+                    })
+                  }}
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ marginTop: 16, width: 256 }}
+                  size="large"
+                  disabled={input.account == '' || input.password == ''}
+                >
+                  {intl('pages.login.buttonMsg')}
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Card>
       </div>
     </div>
   );
